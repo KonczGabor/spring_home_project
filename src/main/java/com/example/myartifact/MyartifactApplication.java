d94a593d4600a93fd379176dda1e97ef263233da
Calindyl <caridniss@gmail.com>
2019-04-19 14:27:09 +0200

    Initial commit

---
+++ src/main/java/com/example/myartifact/MyartifactApplication.java
@@ -0,0 +1,14 @@
+package com.example.myartifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyartifactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyartifactApplication.class, args);
+
+	}
+
+}
